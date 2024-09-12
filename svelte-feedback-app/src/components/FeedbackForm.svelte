17bfe47055802bc2c9afbb474fc6488468db7f7d
Youngjin Shin <yjshin229@gmail.com>
2024-09-12 01:53:20 -0700

    handle rating submit

--- svelte-feedback-app/src/components/FeedbackForm.svelte
+++ svelte-feedback-app/src/components/FeedbackForm.svelte
@@ -1,20 +1,65 @@
 <script>
+  import { v4 as uuidV4 } from 'uuid';
+  import { createEventDispatcher } from 'svelte';
   import Card from './Card.svelte';
   import Button from './Button.svelte';
+  import RatingSelect from './RatingSelect.svelte';
+
+  const dispatch = createEventDispatcher();
 
   let text = '';
+  let rating = 10;
   let btnDisabled = true;
+  let message;
+
+  const handleInput = () => {
+    if (text.trim().length <= 10) {
+      message = 'Text must be at least 10 characters';
+      btnDisabled = true;
+    } else {
+      message = null;
+      btnDisabled = false;
+    }
+  };
+
+  const handleSelect = (e) => {
+    rating = e.detail;
+  };
+
+  const handleSubmit = () => {
+    //just to make sure people arent modifying in dev tools
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        id: uuidV4(),
+        text,
+        rating: +rating,
+      };
+      dispatch('add-feedback', newFeedback);
+      text = '';
+    }
+  };
 </script>
 
 <Card>
   <header>
     <h2>How would you rate your service with us?</h2>
   </header>
-  <form>
+  <form on:submit|preventDefault={handleSubmit}>
+    <RatingSelect on:rating-select={handleSelect} />
     <div class="input-group">
-      <input type="text" placeholder="Tell us something that keeps you coming back" />
+      <input
+        type="text"
+        placeholder="Tell us something that keeps you coming back"
+        on:input={handleInput}
+        bind:value={text}
+      />
       <Button type="submit" disabled={btnDisabled}>Send</Button>
     </div>
+    {#if message}
+      <div class="message">
+        {message}
+      </div>
+    {/if}
   </form>
 </Card>
 
