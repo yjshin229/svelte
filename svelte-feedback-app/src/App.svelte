0e34c22f95c8b54f1e2dfc8d6c21f42b60fd162d
Youngjin Shin <yjshin229@gmail.com>
2024-09-12 00:07:29 -0700

    svelte basics

--- svelte-feedback-app/src/App.svelte
+++ svelte-feedback-app/src/App.svelte
@@ -1,10 +1,45 @@
 <script>
-	export let name;
+	let firstName = 'Youngjin';
+	let lastName = 'Shin';
+
+	let color = 'blue'
+	let showText = false;
+	
+	let users = [
+		{
+			id: '1',
+			name: 'John',
+		},
+		{
+			id: '2',
+			name: 'Sara',
+		},
+		{
+			id: '3',
+			name: 'Bob',
+		}
+	]
+
+	$: name = firstName + ' ' + lastName;
+
+	const toggle = () => {
+		color = color === 'blue'? 'red' : 'blue'
+		showText = !showText;
+		users = [...users, {id:'4', name:'Jen'}]
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1 style="color: {color}">Hello {name}!</h1>
+	{#if showText}
+		
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{/if}
+
+	<button on:click={toggle}>Click</button>
+	{#each users as user (user.id)}
+		<h3>{user.id}: {user.name}</h3>
+	{/each}
 </main>
 
 <style>
